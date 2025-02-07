a97ac26af37d39b9ad75353eef1daebdec16429e
Brian K Jones <135661163+Holden0905@users.noreply.github.com>
2025-02-07 15:42:47 -0600

    found duplicate files in project.

---
+++ App.js
@@ -0,0 +1,13 @@
+import PDFViewer from './components/PDFViewer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PDF Viewer</h1>
+      <PDFViewer />
+    </div>
+  );
+}
+
+export default App; 
